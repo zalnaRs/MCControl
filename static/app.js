094150fa404875e570f04bc1c050e48e8eef378b
zalnaRs <zalnars@proton.me>
2025-02-04 19:47:13 +0100

    Ensure trailing slash in URL redirects.
    Add a check to append a trailing slash to the URL if missing. This ensures consistent URL formatting and prevents potential routing issues.

--- static/app.js
+++ static/app.js
@@ -8,6 +8,9 @@ window.API_URL = {
     logs: {url: `${BASE_URL}logs`, method: "GET"},
 };
 
+if (!window.location.href.endsWith("/"))
+    location.href += "/";
+
 window.onload = async () => {
     const mainView = document.querySelector("#main_view")
     const loginView = document.querySelector("#login_view")
